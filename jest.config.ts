275fe1794828d7b3958eb45bfcce58b2e6046f8f
hcy08 <sweet17strawberry@gmail.com>
2025-06-14 12:45:18 +0900

    chore: update jest configuration for coverage reporting

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ import type { Config } from 'jest';
 const config: Config = {
   clearMocks: true,
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testMatch: [
     '**/__tests__/**/*.test.[jt]s?(x)',
     '**/?(*.)+(spec|test).[jt]s?(x)',
